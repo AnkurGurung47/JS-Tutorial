1d9b073dd22c4b79bbe2520e18c2c780efc5069e
AnkurGurung47 <xyz527292@gmail.com>
2023-11-08 14:56:12 +0000

    Varible Added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=12 4;
+let accountEmail="xyz@gmail.com";
+var password='12345';
+accountCity="Dehradun" //don't do this
+
+// accountId=2; //not allowed
+accountEmail="xy@gmail.com";
+password='321';
+accountCity="delhi";
+console.log(accountId);
+
+console.table([accountEmail,password,accountCity]); //.table method
