511c4aa5efc00a076b2aee4ae7eb264ea26330fd
AnkurGurung47 <xyz527292@gmail.com>
2023-11-10 13:04:46 +0000

    New file added

---
+++ 01_basics/03_dataTypesConfusion.js
@@ -0,0 +1,18 @@
+console.log("1"+1);
+console.log(1+"1");
+console.log("1"+1+2);
+console.log(1+1+"2");
+console.log(true);
+console.log(false);
+console.log(+"");
+console.log(+true);
+console.log(+false);
+
+//postfix/prefix
+
+let num=5;
+console.log(num++);
+let num1=5;
+
+console.log(++num1);
+
